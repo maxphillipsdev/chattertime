0101f17d54cd708fb05e2ada89c3e8b3a8d9d244
Max Phillips <max@maxphillipsdev.com>
2021-11-05 20:06:15 +1100

    Add first test

---
+++ frontend/cypress/integration/index.spec.ts
@@ -0,0 +1,8 @@
+describe("Test Homepage", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+  it("Has a title", () => {
+    cy.get("h1").contains("Welcome to Chatterti.me");
+  });
+});
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 const HomePage: NextPage = () => {
   return (
     <div>
+      <h1>Welcome to Chatterti.me</h1>
       <p>hello world!</p>
     </div>
   );
