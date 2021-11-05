be8a8206d52f0e4dcc26c09af95a5ad14f527982
Max Phillips <max@maxphillipsdev.com>
2021-11-05 19:21:33 +1100

    More project setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig}  */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
