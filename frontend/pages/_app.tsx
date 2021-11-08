21c3e3eeab9961954f4d8ae503bcc8f437fed2ab
Max Phillips <max@maxphillipsdev.com>
2021-11-08 17:23:46 +1100

    Removed gun stuff

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,19 +1,10 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import GunContext from "../contexts/GunContext";
-
-import GUN from "gun";
-import "gun/sea";
-import "gun/axe";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const db = GUN("ws://localhost:5000/gun");
-
   return (
     <>
-      <GunContext.Provider value={{ db }}>
-        <Component {...pageProps} />
-      </GunContext.Provider>
+      <Component {...pageProps} />
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig}  */
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
